920a4ad8be356bb47b21ef4cc1744b1498e4dfec
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-23 20:02:46 +0800

    Moved definition to cpp file and removed template usage

--- src/Board.cpp
+++ src/Board.cpp
@@ -1,1 +1,23 @@
 #include "Board.hpp"
+
+Board::Board(int size)
+{
+    boardContents.reserve(size);
+    int evenOffset = (size % 2 == 0) ? 1 : 0;
+    int xStartPos = evenOffset;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (xStartPos >= size)
+        {
+            xStartPos = xStartPos - size - (evenOffset);
+        }
+        boardContents[i] = Vector2(i, xStartPos);
+        xStartPos += 2;
+    }
+
+    for (const Vector2 &pos : boardContents)
+    {
+        std::cout << pos.x << " " << pos.y << std::endl;
+    }
+}
--- src/Board.hpp
+++ src/Board.hpp
@@ -1,5 +1,5 @@
 #pragma once
-#include <array>
+#include <vector>
 #include <iostream>
 
 struct Vector2
@@ -12,31 +12,11 @@ struct Vector2
     int y;
 };
 
-template <std::size_t N> class Board
+class Board
 {
   public:
-    Board()
-    {
-        int evenOffset = (N % 2 == 0) ? 1 : 0;
-        int xStartPos = evenOffset;
-        
-        for (int i = 0; i < N; i++)
-        {
-            if (xStartPos >= N)
-            {
-                xStartPos = xStartPos - N - (evenOffset);
-            }
-            boardContents[i] = Vector2(i, xStartPos);
-            xStartPos += 2;
-        }
-
-        for (const Vector2 &pos : boardContents)
-        {
-            std::cout << pos.x << " " << pos.y << std::endl;
-        }
-    }
+    Board(int size);
 
   private:
-    std::array<Vector2, N> boardContents;
-    int nMax;
+    std::vector<Vector2> boardContents;
 };
