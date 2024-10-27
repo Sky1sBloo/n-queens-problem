2bb1027c43b03119bf0319adf0a39fe15d141033
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-27 20:41:26 +0800

    Added positin intersects

--- src/QueenBuilder.cpp
+++ src/QueenBuilder.cpp
@@ -1,7 +1,38 @@
 #include "QueenBuilder.hpp"
 
-
-QueenBuilder::QueenBuilder(std::size_t size) :
-    positions({}, size)
+QueenBuilder::QueenBuilder(std::size_t size)
+    : positions({}, size)
 {
+    int currentRow = 0;
+    int newNodeStartCol = (positions.isEmpty()) ? 0 : (positions.top().x + 2) % size;  // Starting column of new node
+
+    while (true) {
+        Vector2 newNodePos(newNodeStartCol, currentRow);
+
+        if (positionIntersects(newNodePos)) {
+            if (++newNodePos.x == newNodeStartCol) {
+                positions.pop();
+                currentRow--;
+                newNodeStartCol++;
+            }
+
+            positions.pop();
+        } else {
+            positions.push(newNodePos);
+            newNodeStartCol = (newNodePos.x + 2) % size;
+        }
+    }
+}
+
+bool QueenBuilder::positionIntersects(const Vector2& newPosition)
+{
+    for (const Vector2& iPos : positions) {
+        if (newPosition.x == iPos.x || newPosition.y == iPos.y) {
+            return false;
+        }
+        if (newPosition.y - iPos.y == newPosition.x - iPos.x) {
+            return false;
+        }
+    }
+    return true;
 }
--- src/QueenBuilder.hpp
+++ src/QueenBuilder.hpp
@@ -18,6 +18,7 @@ struct Vector2 {
 class QueenBuilder {
 public:
     QueenBuilder(std::size_t size);
+    bool positionIntersects(const Vector2& newPosition);
 
     friend std::ostream& operator<<(std::ostream& os, const QueenBuilder& board);
 
