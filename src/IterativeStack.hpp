fb555d8b9a175b01dcca3b7b903d01e9b301798b
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-27 19:53:34 +0800

    Moved function declarations to header

--- src/IterativeStack.hpp
+++ src/IterativeStack.hpp
@@ -1,16 +1,47 @@
 #pragma once
 
-#include <initializer_list>
 #include <array>
+#include <initializer_list>
+#include <stdexcept>
 
 template <typename T, int Size>
 class IterativeStack {
 public:
     IterativeStack(std::initializer_list<T>);
 
-    T& top();
-    void push(const T& newValue);
-    void pop();
+    T& top()
+    {
+        return values[current];
+    }
+
+    void push(const T& newValue)
+    {
+        if (length == Size - 1) {
+            throw std::length_error("Tried pushing a full stack");
+        }
+
+        if (current < Size - 1) {
+            values[++current] = newValue;
+        } else {
+            values[0] = newValue;
+        }
+
+        length++;
+    }
+
+    void pop()
+    {
+        if (length == 0) {
+            throw std::length_error("Tried popping an empty stack");
+        }
+
+        if (current > 0) {
+            current--;
+            length--;
+        } else {
+            current = (--length > 0) ? (Size - 1) : 0;
+        }
+    }
 
 private:
     std::array<T, Size> values;
