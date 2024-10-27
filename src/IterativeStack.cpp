d68d4f4aa729bebd351719938a5a2c7a3a993721
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-27 19:41:23 +0800

    Changed pop length checking

--- src/IterativeStack.cpp
+++ src/IterativeStack.cpp
@@ -1,4 +1,5 @@
 #include "IterativeStack.hpp"
+#include <stdexcept>
 
 template <typename T, int Size>
 T& IterativeStack<T, Size>::top()
@@ -14,16 +15,13 @@ void IterativeStack<T, Size>::push(const T& newValue)
 template <typename T, int Size>
 void IterativeStack<T, Size>::pop()
 {
-    auto decrementChecker = [&](int newValue) {
-        if (length - 1 >= 0) {
-            current = newValue;
-            length--;
-        }
-    };
+    if (length == 0) {
+        throw std::out_of_range("Tried popping an empty stack");
+    }
 
     if (current > 0) {
-        decrementChecker(--current);
+        current--;
     } else {
-        decrementChecker(Size - 1);
+        current = Size - 1;
     }
 }
