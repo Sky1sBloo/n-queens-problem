5148db3faab5fdb5562bc3773c7c98c6dbc2a6a4
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-28 21:10:29 +0800

    Restored diagonal checking that is wrong

--- src/QueenBuilder.cpp
+++ src/QueenBuilder.cpp
@@ -39,7 +39,7 @@ bool QueenBuilder::positionIntersects(const Vector2& newPosition)
         if (newPosition.x == iPos.x || newPosition.y == iPos.y) {
             return true;
         }
-        if (newPosition.y - iPos.y == newPosition.x - iPos.x) {
+        if (std::abs(newPosition.y - iPos.y) == std::abs(newPosition.x - iPos.x)) {
             return true;
         }
     }
