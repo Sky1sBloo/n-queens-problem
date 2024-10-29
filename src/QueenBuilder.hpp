0e22946f41e81194c72dbdb4b2dfdaa84f36a3c4
Sky1sBloo <ultimate8gaming@gmail.com>
2024-10-29 18:34:08 +0800

    Added documentation

--- src/QueenBuilder.hpp
+++ src/QueenBuilder.hpp
@@ -4,9 +4,13 @@
 
 #include "IterativeStack.hpp"
 
+/**
+ * Struct representing a queen position
+ */
 struct Position {
-    Position(int newColumn, int newRow) :
-        column(newColumn), row(newRow)
+    Position(int newColumn, int newRow)
+        : column(newColumn)
+        , row(newRow)
     {
     }
 
@@ -14,17 +18,38 @@ struct Position {
     int row;
 };
 
+/**
+ * Class used to generate the n-queens-problem
+ *
+ * Throws QueenBuilderException if it cannot find a solution
+ */
 class QueenBuilder {
 public:
+    /**
+     * Positions is generated in here
+     */
     QueenBuilder(std::size_t size);
-    bool topPositionIntersects();
 
+    /**
+     * Used to retrieve position data
+     * Can be stored using std::stringstream
+     */
     friend std::ostream& operator<<(std::ostream& os, QueenBuilder& board);
 
 private:
     IterativeStack<Position> positions;
+
+    /**
+     * Checks if the top of stack intersects with the previous ones
+     *
+     * @return True if the top is horizontally, vertically, or diagonally aligned
+     */
+    bool topPositionIntersects();
 };
 
+/**
+ * Exception that gets thrown when QueenBuilder cannot generate solutions;
+ */
 class QueenBuilderException : public std::exception {
 public:
     const char* what()
